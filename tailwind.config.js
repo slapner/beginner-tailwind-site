c7583ab5eb5e6e73291ed5153bfdb0ca1caa637e
slapner <bclap4@gmail.com>
2020-12-21 19:19:08 -0500

    quick styles for deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['src/**/*.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				teal: colors.teal,
+				orange: colors.orange,
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
